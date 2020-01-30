6e2920b490b0c92292f2ab5b10f3e30e6edcc479
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-30 17:34:46 -0500

    form validation complete

--- js/formValidator.js
+++ js/formValidator.js
@@ -1,1 +1,67 @@
-console.log('linked')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, msg) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.textContent = msg;
+}
+
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const valid = re.test(String(input.value.trim()).toLowerCase());
+  if (valid) {
+    showSuccess(input)
+  } else {
+    showError(input, 'This email looks fake to me');
+  }
+}
+
+function matchPassword(input1, input2) {
+  input1.value.trim() === input2.value.trim() ? showSuccess(input2) : showError(input2, 'Passwords do not match!')
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkRequired(inputArray) {
+  inputArray.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`)
+    } else {
+      showSuccess(input)
+    }
+  });
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min ) {
+    showError(input, `${getFieldName(input)} must be atleast ${min} characters`)
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+  } else {
+    showSuccess(input);
+  }
+}
+
+
+// Event Listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  matchPassword(password, password2)
+})
